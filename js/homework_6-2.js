d3a559b4ac407c1a5e2f38eea67d67ae2d21be8f
DeZMonD251 <83780168+DeZMonD251@users.noreply.github.com>
2021-11-08 14:13:33 +0200

    Add files via upload

--- js/homework_6-2.js
+++ js/homework_6-2.js
@@ -2,14 +2,18 @@
 количество дней до Нового года от date, если оно задано, 
 иначе посчитать количество дней от текущей даты*/
 function get_count_days(date) {
-    now = new Date();
-    ny = new Date(now.getFullYear() + 1, 0, 1, 0, 0, 0);
+  now = new Date();
+  ny = new Date(now.getFullYear() + 1, 0, 1, 0, 0, 0);
+  if (isNaN(date)) {
+    date = new Date();
+    return Math.floor((ny.getTime() - date.getTime()) / 86400000);
+  } else {
     if (date instanceof Date) {
-        date = new Date();
-        return Math.floor((ny.getTime() - date.getTime()) / 86400000);
+      return Math.floor((ny.getTime() - date.getTime()) / 86400000);
     } else {
-        return Math.floor((ny.getTime() - date.getTime()) / 86400000);
+      return "Error";
     }
+  }
 }
-date_1 = new Date(2021,10, 22);
+date_1 = new Date(2021, 10, 22);
 console.log(get_count_days());
