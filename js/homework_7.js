b3b264ad2f0f09ee225da1636bb48de9e4a7b224
DeZMonD251 <83780168+DeZMonD251@users.noreply.github.com>
2021-11-10 16:29:27 +0200

    Add files via upload

---
+++ js/homework_7.js
@@ -0,0 +1,81 @@
+function Calculator(name){
+    this.name = name;
+    history = ' ';
+    this.addition = function () {
+        sum = 0;
+        arg = '(';
+        for (let i = 0; i < arguments.length; i++){
+            if (typeof(arguments[i])  === 'number') {
+                sum += arguments[i];
+            } else {
+                sum = 'Error';
+                break;
+            }
+            if (i === arguments.length - 1) {
+                arg += arguments[i].toString() + ')';
+            } else {
+                arg += arguments[i].toString() + ',';
+            }
+        }
+        date = new Date();
+        history += this.name + ' ' + date.toLocaleDateString() + ' ' + date.toLocaleTimeString() + ': сумма = ' + sum + arg + '\n';
+        console.log(sum);
+    };
+    this.multiplication = function () {
+        pr = 1;
+        arg = '(';
+        for (let i = 0; i < arguments.length; i++){
+            if (typeof(arguments[i])  === 'number') {
+                pr *= arguments[i];
+            } else {
+                pr = 'Error';
+                break;
+            }
+            if (i === arguments.length - 1) {
+                arg += arguments[i].toString() + ')';
+            } else {
+                arg += arguments[i].toString() + ',';
+            }
+        }
+        date = new Date();
+        history += this.name + ' ' + date.toLocaleDateString() + ' ' + date.toLocaleTimeString() + ': умножение = ' + pr + arg + '\n';
+        console.log(pr);
+    };
+    this.subtraction = function (a, b) {
+        result = a - b;
+        if (typeof(a) === 'number' && typeof(b) === 'number') {
+            console.log(result);
+        } else {
+            console.log('Error!');
+        }
+        arg = '(' + a.toString() + ',' + b.toString() + ')'; 
+        date = new Date();
+        history += this.name + ' ' + date.toLocaleDateString() + ' ' + date.toLocaleTimeString() + ': вычитаение = ' + result + arg + '\n';
+    };
+    this.division = function (a, b) {
+        result = a / b;
+        if (typeof(a) === 'number' && typeof(b) === 'number') {
+            console.log(result);
+        } else {
+            console.log('Error!');
+        }
+        arg = '(' + a.toString() + ',' + b.toString() + ')'; 
+        date = new Date();
+        history += this.name + ' ' + date.toLocaleDateString() + ' ' + date.toLocaleTimeString() + ': вычитаение = ' + result + arg + '\n';
+    }
+    this.showHistory = function () {
+        console.log(history);
+    };
+    this.deleteHistory = function () {
+        history = ' ';
+    }
+}
+calculator = new Calculator('Calculator');
+console.log(calculator);
+calculator.addition(1,12,2,6,3,5,3,8);
+calculator.multiplication(1,4,2,5,2,7);
+calculator.subtraction(56, 75);
+calculator.division(56, 75);
+calculator.showHistory();
+calculator.deleteHistory();
+calculator.showHistory();
